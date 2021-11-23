31182ee0127d06a6033bcc416ba5c2f003ee535c
dell <2668714636@qq.com>
2021-11-23 23:19:09 +0800

    104 settings

---
+++ train-search.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=2 python train_search.py \
+ --batch-size 4 --dataset GID --checkname 12layers \
+ --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 256 \
+ --resume  /media/data/wy/Seg_NAS/run/GID/12layers/model_best.pth.tar
