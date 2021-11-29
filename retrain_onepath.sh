adc8eabb367ed2dcdb3768408324a49c643866dc
wy <faye892322@gmail.com>
2021-11-30 00:54:31 +0800

    统一了模型训练，添加test模块

--- retrain_onepath.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 python retrain_nas.py \
- --batch-size 10 --dataset GID --checkname 12layers_onepath_retrain --num_worker 4\
- --alpha_epoch 10 --resize 512 --crop_size 512 \
- --epochs 100  --use_default False\
